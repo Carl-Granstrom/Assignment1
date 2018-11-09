dd327b1f3eae244d27c9acf0d8e8ce0361957aed
Carl-Granstrom <35761913+Carl-Granstrom@users.noreply.github.com>
2018-11-09 21:39:29 +0100

    Bleurgh

--- src/stud/ltu/Fakultet.java
+++ src/stud/ltu/Fakultet.java
@@ -1,40 +1,61 @@
+/*************************************************************************
+ *  Syfte: Programmet hanterar beräkningar av Fakultetsfunktionen. Programmet hanterar felaktig
+ *  input från användaren i form av negativa tal, fakultetsfunktionen växer så snabbt att
+ *  även long drabbas av overflow-problem. i praktiken hanterar inte programmet fakultet högre än 20
+ *  sedan blir resultatet för stort för long.
+ *
+ *  Föbättring: BigInteger skulle kunna användas för att hantera ännu större tal.
+ *
+ *  Programmet skrevs som en del i Systemvetenskapsutbildningen på Luleå Tekniska Högskola(LTU)
+ *  inom kursen D0019N - Programutveckling med Java. Uppgift 1.1.
+ *
+ *  Författare: Carl Granström
+ *
+ *  Datum: 2018-11-07
+ *
+ *************************************************************************/
+
 package stud.ltu;
 
 import java.util.Scanner;
 
 public class Fakultet {
 
-    static private Scanner sc = new Scanner(System.in);
-    static private int n = inputTal();
+    static private Scanner sc = new Scanner(System.in);  //skapar en Scanner för att kunna läsa input
+    static private long n = inputTal();  //använder long eftersom fakultet snabbt blir väldigt stor
 
     Fakultet(){}
 
-    public static int getN() {
+    public static long getN() {
         return n;
+    }  //metoden behövs inte för nuvarande implementation, men är bra träning
+
+    private static long inputTal() {
+        System.out.println("Vänligen skriv in ett naturligt tal för beräkning av fakultet: ");
+        n = sc.nextInt(); //läs in int från användaren
+        while (!accTal(n)) {
+            System.out.println("Inte negativt, men inte heller över 20, vänligen skriv ett nytt tal");
+            n = sc.nextInt();
+        }
+        return n;  //implicit konvertering från int till long
     }
 
-    private static int inputTal() {
-        System.out.println("Vänligen skriv in ett naturligt tal för beräkning av fakultet: ");
-        n = sc.nextInt();
-        while (!naturligtTal(n)) {
-            System.out.println("Fakultet är endast definierat för naturliga tal, vänligen försök igen");
-            n = sc.nextInt();
+    private static long beraknaFakultet(long n) {
+        //Fakultet 0(0!) = 1
+        if (n == 0) {
+            return 1;
+        }
+        //Beräkna fakultet rekursivt
+        if (n > 1) {
+            n = (n * beraknaFakultet(n - 1));
+        } else {
+            n *= n;
         }
         return n;
     }
 
-    private static int beraknaFakultet(int n) {
-
-        int resultat;
-
-        //placeholder method
-        resultat = 1;
-
-        return resultat;
-    }
-
-    private static boolean naturligtTal(int n) {
-        return (n >= 0);
+    private static boolean accTal(long n) {
+        return (n >= 0 && n < 21);
     }
 
     public static void main(String[] args){
