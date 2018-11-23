f27284fc83dcfb89908f4ed7b7edfd860b7f8b64
Carl Granstrom <carl.grnstrm@gmail.com>
2018-11-24 00:16:57 +0100

    Döpt om några fält.

--- src/stud/ltu/WakeUpGym/SalsRegister.java
+++ src/stud/ltu/WakeUpGym/SalsRegister.java
@@ -6,19 +6,19 @@ import java.util.ArrayList;
  * Testar Singleton-pattern
  */
 public final class SalsRegister {
-    private static SalsRegister ourInstance = new SalsRegister();
+    private static SalsRegister singleRegister = new SalsRegister();
+
+    ArrayList<Sal> register = new ArrayList<>();
 
     public static SalsRegister getInstance() {
-        return ourInstance;
+        return singleRegister;
     }
 
     /**
      * Skapar i nuläget några salar statiskt som en placeholder för test.
      */
     private SalsRegister() {
-        ArrayList<Sal> register = new ArrayList<>();
-
-        register.add(new Sal("Lotusblomman",3,3));      //placeholder
+        register.add(new Sal("Lotusblomman",3,3));      //ph, move to method
         register.add(new Sal("Himalaya", 5, 5));        //ph
         register.add(new Sal("Lilla Nirvana", 4, 6));   //ph
     }
