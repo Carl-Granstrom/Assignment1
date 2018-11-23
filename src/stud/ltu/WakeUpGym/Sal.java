d1f37ca0cd5767a13421f207815001fc402c8831
Carl Granstrom <carl.grnstrm@gmail.com>
2018-11-24 00:16:25 +0100

    Implementerat namngivningslogiken för Platserna i salen. Platserna skapas och namnges automatiskt när ett nytt Sal-objekt skapas.

--- src/stud/ltu/WakeUpGym/Sal.java
+++ src/stud/ltu/WakeUpGym/Sal.java
@@ -11,8 +11,8 @@ public class Sal {
     private int rader;
     private int platserPerRad;
     //Använder inte ArrayList då jag inte vill att platserna ska flyttas runt i arrayen med några list-metoder.
-    private final Plats[] platser = new Plats[rader * platserPerRad + 1]; //skippar 0-index
-
+    private final Plats[] platser = new Plats[rader * platserPerRad]; //skapa array med antal platser (null init)
+    char[] alfabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
     /**
      * @param namn          salens namn
      * @param rader         antal rader i salen
@@ -22,6 +22,13 @@ public class Sal {
         this.namn = namn;
         this.rader = rader;
         this.platserPerRad = platserPerRad;
+        //fyll platser-array med Plats-objekt med rätt namn(1a, 1b, 1c, 2a, 2b, 2c)
+        for (int i = 0; i < platser.length; i++){
+            int rad = (i / platserPerRad) + 1;
+            int alfabetsIndex = i % platserPerRad;
+            char plats = alfabet[alfabetsIndex];
+            platser[i] = new Plats(rad, plats);
+        }
     }
 
     @Override
@@ -37,7 +44,7 @@ public class Sal {
         private char plats;
 
         //Konstruktor
-        //TODO Kanske kan gå att generera rad och plats automatiskt utifrån indexeringen av objekten i salarna
+        //TODO Kanske kan gå att generera rad och plats automatiskt utifrån indexeringen av Plats:er i salarna
         Plats(int rad, char plats) {
             this.rad = rad;
             this.plats = plats;
