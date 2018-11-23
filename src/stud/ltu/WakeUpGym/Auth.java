7c595047e75f0b0aae31cfcdbc47d79510d288e6
Carl Granstrom <carl.grnstrm@gmail.com>
2018-11-23 15:07:29 +0100

    Implementerat logiken för personnummer-algon beskriven i A2-instruktionerna. Saknas fortfarande bra kontroller av om input är laglig och tillhörande throws!

--- src/stud/ltu/WakeUpGym/Auth.java
+++ src/stud/ltu/WakeUpGym/Auth.java
@@ -1,4 +1,29 @@
 package stud.ltu.WakeUpGym;
 
+import java.util.stream.IntStream;
+
+/**
+ * Några saker behöver kollas angående personnumret som slagits in, annars throw exception, och hantera detta
+ * längre upp i programmet: endast 0-9 i char array, char array 10 lång.
+ */
 public class Auth {
+
+
+    Auth(Medlem medlem){
+
+    }
+
+    //precondition: char array c[] är 10 chars lång
+    private static boolean pnrIsLegal(char[] c) {
+        int[] numbers = new int[10];
+        //convert char array to int array
+        for (int i = 0; i < 10; i++){
+            numbers[i] = Character.getNumericValue(c[i]); //return numeric val of char at index i, store in numbers[i]
+        }
+
+        //multiplicera med 2 och addera, IntStream-implementation, Deitel & Deitel (s. 762)
+        int sum = IntStream.of(numbers).map(x -> x * 2).sum();
+
+        return (sum % 10 == 0); // return true if the sum is evenly divisible by 10
+    }
 }
