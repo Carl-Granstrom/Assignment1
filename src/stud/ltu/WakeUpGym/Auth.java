caa5e8dccfdb0f71891a36af8e32469bd106f464
Carl Granstrom <carl.grnstrm@gmail.com>
2018-11-27 23:49:58 +0100

    Jobbat om logiken p.g.a. osnygg algoritm. Dumpade IntStream-implementationen genom att kommentera ut den. Ersatt med vanlig tråk-loop.

--- src/stud/ltu/WakeUpGym/Auth.java
+++ src/stud/ltu/WakeUpGym/Auth.java
@@ -15,10 +15,30 @@ public class Auth {
     //precondition medlemmens int-array personNummer är 10 lång
     public static boolean pnrIsLegal(Medlem medlem) {
         int[] pnr = medlem.getPersonNummer();
+        int sum = 0;
+        /*
+        for(int i : pnr){
+            System.out.println(i);
+        }
+        System.out.println(pnr.length);
+        */
         assert (pnr.length == 10);    //assert precondition is true
 
         //multiplicera med 2 och summera array, IntStream-implementation, Deitel & Deitel (s. 762)
-        int sum = IntStream.of(pnr).map(x -> x * 2).sum();
+            /*
+            commented out bc not suitable to problem
+            int sum = IntStream.of(pnr).map(x -> x * 2).sum();
+            */
+
+        //multiplicera varannan med 2 och summera, foor-loop implementation
+        for (int i = 0; i > 10; i += 2){
+            if (i % 2 == 0){
+                sum += pnr[i] * 2;
+            }
+            else {
+                sum += pnr[i];
+            }
+        }
 
         return (sum % 10 == 0); // return true if the sum is evenly divisible by 10
     }
