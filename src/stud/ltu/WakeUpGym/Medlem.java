fdf600f2beab8c8a6f68e942ad514498ab1b89cb
Carl Granstrom <carl.grnstrm@gmail.com>
2018-11-28 22:45:48 +0100

    Snyggat till koden avsevärt, flyttat ut allt input-skräp från konstruktorn till metoder.

--- src/stud/ltu/WakeUpGym/Medlem.java
+++ src/stud/ltu/WakeUpGym/Medlem.java
@@ -17,44 +17,32 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Medlem {
-    private int[] personNummer = new int[10];  //storing as array also permits leading zeroes
+    private int[] personNummer;  //storing as array also permits leading zeroes
     private String namn;
     private Status medlStatus;
     private Abonnemang abonnemang;
 
-    //inte jättevackert att be om user input i själva konstruktorn, men det får duga TODO rensa upp i skiten
+    //Konstruktor, anropar massa metoder som inte är statiska, men det "bör" inte vara ngt problem.
     Medlem(){
         //initialisera medlemsstatus till inaktiv när ny medlem skapas
         Status medlStatus = Status.INACTIVE;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Vänligen skriv ditt namn och tryck på Enter:");
-        this.namn = sc.nextLine();  //TODO ordna ngn check på namnet så det blir ok. Bryt ut till statisk metod.
-        //TODO bryt ut personnummerhanteringen till statisk metod
-        System.out.println("Vänligen skriv in ditt personnummer som tio siffror och tryck Enter:");
-        String tmpString = sc.nextLine();  //store input String
-        char[] tmpCharArray = tmpString.toCharArray(); //convert String to charArray
-        //move the 10 first characters of the charArray to personNummer int-array.
-        for (int i = 0; i < 10; i++){
-            char c = tmpCharArray[i];
-            int siffra = Character.getNumericValue(c);
-            if (siffra > 9 || siffra < 0) {
-                throw new InputMismatchException("Input contains non-numeric characters!");
-            }
-            personNummer[i] = siffra;  //för in siffran i personNummer array.
-        }
-        System.out.println("Hur många månader vill du vara medlem?\nSkriv in en siffra: ");
-        this.abonnemang = new Abonnemang(sc.nextInt(), medlStatus);
+        this.namn = namnInput();
+        this.personNummer = pnrInput();
+        abonnemang = setAbonnemang(medlStatus);
     }
 
-    //skapa färdig testmedlem för registrering som default i MedlemsRegister
+    /**
+     *  Skapa färdig testmedlem för registrering som default i MedlemsRegister.
+     *  Erik Ladulås är redan medlem så ska inte betala 100kr för det.
+     *  @param b används bara för att undvika standard-konstruktor.
+     */
     Medlem(boolean b){
 
-        this.namn = "Erik Ladulås";
-        int[] tmp = {8, 3, 0, 9, 2, 9, 0, 3, 1, 3};
-        this.personNummer = tmp;
-        //initialisera medlemsstatus till inaktiv när ny medlem skapas
-        this.medlStatus = Status.ACTIVE;
-        this.abonnemang = new Abonnemang(16, this.medlStatus);
+        this.namn = "Erik Ladulås";                                     //Statiskt namn
+        int[] TMP = {8, 3, 0, 9, 2, 9, 0, 3, 1, 3};                     //Pnr-array, garanterat felfri
+        this.personNummer = TMP;                                        //Sätt pnr till ovanstående statiska array
+        this.medlStatus = Status.ACTIVE;                                //Sätt status till aktiv
+        this.abonnemang = new Abonnemang(16, this.medlStatus); //Nytt abonnemang, 16 månader, utan medlskap
     }
 
     public Status getStatus(){
@@ -65,14 +53,43 @@ public class Medlem {
         return abonnemang;
     }
 
-    //TODO inte färdig. Ska anropa input-logiken för antal månader och this.getStatus när detta flyttats ut från konstruktorn
-    public void setAbonnemang(){
-        //skapa ett nytt abonnemang på Medlem:en
-        this.abonnemang = new Abonnemang(12, Status.INACTIVE);
-    }
-
     public int[] getPersonNummer() {
         return this.personNummer;
+    }
+
+    private String namnInput(){
+        if (this.namn == null){
+            Scanner sc = new Scanner(System.in);        //ny scanner
+            System.out.println("Vänligen skriv ditt namn och tryck på Enter:");
+            return sc.nextLine();  //TODO ordna ngn check på namnet så det blir ok.
+        } else {
+            throw new IllegalStateException();
+        }
+    }
+
+    private int[] pnrInput(){
+        int[] pnr = new int[10];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Vänligen skriv in ditt personnummer som tio siffror och tryck Enter:");
+        String tmpString = sc.nextLine();  //store input String
+        char[] tmpCharArray = tmpString.toCharArray(); //convert String to charArray
+        //move the 10 first characters of the charArray to personNummer int-array.
+        for (int i = 0; i < 10; i++){
+            char c = tmpCharArray[i];
+            int siffra = Character.getNumericValue(c);
+            if (siffra > 9 || siffra < 0) {
+                throw new InputMismatchException("Input contains non-numeric characters!");
+            }
+            pnr[i] = siffra;  //för in siffran i personNummer array.
+        }
+        return pnr;
+    }
+
+    //skapa nytt abonnemang
+    private Abonnemang setAbonnemang(Status s){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hur många månader vill du vara medlem?\nSkriv in en siffra: ");
+        return new Abonnemang(sc.nextInt(), s); //skapa ett nytt abonnemang på Medlem:en
     }
 
     @Override
