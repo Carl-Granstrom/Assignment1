a68ea3a459fbce5f8a3442f545104abe27f2afab
Carl Granstrom <carl.grnstrm@gmail.com>
2018-11-28 23:31:46 +0100

    Fixat Auth.pnrIsLegal, hade slarvat när jag läste algon. Igen.
    Resat utkommenterat skräp i koden. Tagit bort felsöknings-print.

--- src/stud/ltu/WakeUpGym/Auth.java
+++ src/stud/ltu/WakeUpGym/Auth.java
@@ -19,20 +19,7 @@ public class Auth {
     public static boolean pnrIsLegal(Medlem medlem) {
         int[] pnr = medlem.getPersonNummer();
         int sum = 0;
-        /*
-        for(int i : pnr){
-            System.out.println(i);
-        }
-        System.out.println(pnr.length);
-        */
         assert (pnr.length == 10);    //assert precondition is true
-
-        //multiplicera med 2 och summera array, IntStream-implementation, Deitel & Deitel (s. 762)
-            /*
-            commented out bc not suitable to problem
-            int sum = IntStream.of(pnr).map(x -> x * 2).sum();
-            */
-
         //multiplicera varannan med 2 och summera, foor-loop implementation
         for (int i = 0; i < 10; i++){
             if (i % 2 == 0){
