877610ef3f8f2345e27341ace0bc82d1dfce84f7
Carl Granstrom <carl.grnstrm@gmail.com>
2018-11-28 23:30:31 +0100

    Fixat Auth.pnrIsLegal, hade slarvat när jag läste algon. Igen.

--- src/stud/ltu/WakeUpGym/Auth.java
+++ src/stud/ltu/WakeUpGym/Auth.java
@@ -12,7 +12,10 @@ public class Auth {
     private Auth(){
     }
 
-    //precondition medlemmens int-array personNummer är 10 lång
+    /**
+     * precondition medlemmens int-array personNummer är 10 lång
+     * TODO kolla att algon funkar som den ska, verkar eventuellt returnera true jämt
+     */
     public static boolean pnrIsLegal(Medlem medlem) {
         int[] pnr = medlem.getPersonNummer();
         int sum = 0;
@@ -31,15 +34,19 @@ public class Auth {
             */
 
         //multiplicera varannan med 2 och summera, foor-loop implementation
-        for (int i = 0; i > 10; i += 2){
+        for (int i = 0; i < 10; i++){
             if (i % 2 == 0){
-                sum += pnr[i] * 2;
-            }
-            else {
+                int tmp = pnr[i] * 2;
+                if (tmp < 10) {
+                    sum += pnr[i] * 2;
+                } else {             //hantera 10-18 så att tiotals-ettan endast räknas som 1 i summeringen
+                    sum += 1;
+                    sum += tmp % 10;
+                }
+            } else {
                 sum += pnr[i];
             }
         }
-
         return (sum % 10 == 0); // return true if the sum is evenly divisible by 10
     }
 }
